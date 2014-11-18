7e7d2367a0aece2256814e1c4d773831d2941dad
Valentin <valio26@email.bg>
2014-11-18 22:03:40 +0200

    dda1

---
+++ zadacha_7.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include <iostream>
+int main();
+{   int hlqb;
+    cin >> "cena = 1 lv." >> "broi = 3" >> endl;
+	cin >> "DDS = 20%";
+	cout << stoinost na DDS;
+	return o;
+}
+
